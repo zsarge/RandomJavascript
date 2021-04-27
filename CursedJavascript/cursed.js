d016e007b9a119752625143e37f46556f846f788
zsarge <zackarywsargent@gmail.com>
2021-04-26 22:36:14 -0400

    add examples of cursed javascript

---
+++ CursedJavascript/cursed.js
@@ -0,0 +1,30 @@
+`use strict`;
+/* 
+ * Cursed Javascript: Javascript that pains the eyes and soul.
+ * Zack Sargent
+ */
+
+// Not cursed, just a separator
+const title = string => console.log(`\n# ${string}`)
+
+title("Rebinding keywords:")
+
+let = console.log
+let("Hello World!") == 3
+
+/* The following are translations of 
+ * https://stackoverflow.com/q/1642028
+ */
+title("The `--> operator`")
+// actually a combination of `--` and `>`
+
+high = 10;
+while (high --> 0)
+	console.log(high)
+
+title("Decrementing by two")
+x = 10;
+
+for(x++;--x;x--)
+    console.log(x)
+
