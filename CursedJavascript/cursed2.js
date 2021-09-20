120648ce07509b4ba7747ac8164e4022bbf04022
zsarge <zackarywsargent+github@gmail.com>
2021-09-20 11:09:47 -0400

    format code better (?)

--- CursedJavascript/cursed2.js
+++ CursedJavascript/cursed2.js
@@ -2,10 +2,17 @@
 
 
 function countExclamationMarks(str) {
-    return !eval(Array.from(str).filter(char => char === '!').join("") + "0") ? "even !'s" : "odd !'s";
+    return !eval(
+        Array
+            .from(str)
+            .filter(char => char === '!')
+            .join("") + "0"
+    ) 
+    ? "even !'s" 
+    : "odd !'s"
 }
 
-console.log( countExclamationMarks("!!!"))
-console.log( countExclamationMarks("!!!!"))
+console.log(countExclamationMarks("!!!"))
+console.log(countExclamationMarks("!!!!"))
 
 
